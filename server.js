0f821fb888c2c2978c10688d3b7dec939a447715
bordedelcielo <christopher.thrutchley@gmail.com>
2022-08-02 16:28:36 -0500

    minor tweaks

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = express()
 
 connectDB()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", (req, res) => {
+		res.send('Hello World')
 })
 
 app.listen(process.env.PORT, () => {
     console.log(`Server now running on PORT ${process.env.PORT}`)
-})
+});
