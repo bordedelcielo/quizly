8059772dc195405085cb895b2478694d8d1f5502
bordedelcielo <christopher.thrutchley@gmail.com>
2022-08-03 10:47:53 -0500

    updated server

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const { connectDB } = require('./src/db')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./src/graphqlschema')
 
 dotenv.config()
 
 const app = express()
 
 connectDB()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true // graphiql?
+}))
 
 app.get("/", (req, res) => {
 		res.send('Hello World')
