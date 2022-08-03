8270f0879521893aba21aa1a3f2f860788e51ceb
bordedelcielo <christopher.thrutchley@gmail.com>
2022-08-03 17:01:29 -0500

    middleware, templates

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv')
 const { connectDB } = require('./src/db')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./src/graphql/schema')
+const cookieParser = require('cookie-parser')
+const { authenticate } = require('./src/middleware/auth')
+const path = require('path')
 
 dotenv.config()
 
@@ -10,10 +13,20 @@ const app = express()
 
 connectDB()
 
+app.use(cookieParser())
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true // graphiql?
 }))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, '/src/templates/views'));
+
+app.use(authenticate)
 
 require('./src/routes')(app)
 
