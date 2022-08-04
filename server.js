f5f9612112a0a4a66cd10340e4676623245625ae
bordedelcielo <christopher.thrutchley@gmail.com>
2022-08-04 13:38:35 -0500

    hitting error, troubleshooting

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const dotenv = require('dotenv')
 const { connectDB } = require('./src/db')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./src/graphql/schema')
-const cookieParser = require('cookie-parser')
 const { authenticate } = require('./src/middleware/auth')
+const { userData } = require('./src/middleware/userData')
 const path = require('path')
+const cookieParser = require('cookie-parser')
 
 dotenv.config()
 
@@ -13,22 +14,25 @@ const app = express()
 
 connectDB()
 
+app.use(authenticate)
+app.use(userData)
 app.use(cookieParser())
 
-app.use('/graphql', graphqlHTTP({
+app.use("/graphql", graphqlHTTP({
     schema,
-    graphiql: true // graphiql?
+    graphiql: true
 }))
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// update location of views folder that res.render pulls from
+app.set('views', path.join(__dirname, '/src/templates/views'));
 
 app.use(express.urlencoded({ extended: true }))
 
-app.set('view engine', 'ejs');
-
-app.set('views', path.join(__dirname, '/src/templates/views'));
-
-app.use(authenticate)
-
-require('./src/routes')(app)
+/* Initialize Routes */
+require("./src/routes")(app)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server now running on PORT ${process.env.PORT}`)
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken")
 const unprotectedRoutes = [
-    '/auth/login',
-    '/auth/register',
-    '/graphql'
+    "/auth/login",
+    "/auth/register",
+    "/graphql"
 ]
 
 const authenticate = async (req, res, next) => {
-    const token = req.cookies?.jwToken || ""
+    const token = req.cookies?.jwtToken || ""
 
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET)
         req.verifiedUser = verified
-        console.log('User verification successful!', verified)
+        console.log("User verification successful!", verified)
         next()
     } catch(err) {
-        console.log('User verification failed')
+        console.log("User verification failed")
 
-        if ( unprotectedRoutes.includes(req.path)) {
+        if ( unprotectedRoutes.includes(req.path) ) {
             next()
         } else {
             res.redirect(`/auth/login`)
